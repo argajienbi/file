fabbe84da533c65d8592411f8a97cf6ada01cdd0
argajienbi <armin.gandi@gmail.com>
2019-03-20 00:24:21 +0700

    Add files via upload

---
+++ file.php
@@ -0,0 +1,9 @@
+<?php
+/* setting bot */
+
+$gluward=$ijo."
+╔═╗──────────────────╔╗
+║╬║╔╗─╔╦╗╔╦╦╗╔═╗─╔╦╗╔╝║
+╠╗║║╚╗║║║║║║║║╬╚╗║╔╝║╬║
+╚═╝╚═╝╚═╝╚══╝╚══╝╚╝─╚═╝.
+creator:$ijo armin gandi $putih || Yuotube Chanel:$ijo Armin Gandi$putih";
