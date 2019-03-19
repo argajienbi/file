507155c201a1bccc6d6b3440db7b1fc19c78f9f5
argajienbi <armin.gandi@gmail.com>
2019-03-20 00:22:30 +0700

    Update file.php

--- file.php
+++ file.php
@@ -6,7 +6,6 @@ $gluward=$ijo."
 ║╬║╔╗─╔╦╗╔╦╦╗╔═╗─╔╦╗╔╝║
 ╠╗║║╚╗║║║║║║║║╬╚╗║╔╝║╬║
 ╚═╝╚═╝╚═╝╚══╝╚══╝╚╝─╚═╝
-creator:$ijo armin gandi $putih || Yuotube Chanel:$ijo Armin Gandi$putih
-$stat_baca=true;
+pencipta: $ ijo  armin gandi $ putih  || Yuotube Chanel: $ ijo  Armin Gandi $ putih";
 
 ?>
